68eb106d35e890131b3e3b0de59d5ca7793f3a01
xemilyx <emilybrown@cranstonlibrary.org>
2018-01-10 23:51:20 -0500

    Initial commit.

---
+++ DrinksMachinesDemo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace DrinksMachinesDemo
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            DrinksMachine myMachine = new DrinksMachine();
+            myMachine.Location = "kitchen";
+            myMachine.Model = "DM1000";
+
+            Console.WriteLine(myMachine.Location);
+        }
+    }
+
+    class DrinksMachine
+    {
+        private string make;
+        private string model;
+        private string location;
+
+        public string Make { get => make; set => make = value; }
+        public string Model { get => model; set => model = value; }
+        public string Location { get => location; set => location = value; }
+    }
+}
